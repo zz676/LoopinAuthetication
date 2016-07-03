65a1e37732153a2587628802009900e6a4c0f910
zhisheng zhou <zz676@nyu.edu>
2016-07-02 19:50:42 -0500

    First commit

--- config/auth.js
+++ config/auth.js
@@ -1,24 +1,23 @@
 // config/auth.js
 
-// expose our config directly to our application using module.exports
 module.exports = {
 
     'facebookAuth' : {
-        'clientID'        : 'your-secret-clientID-here', // your App ID
-        'clientSecret'    : 'your-client-secret-here', // your App Secret
-        'callbackURL'     : 'http://localhost:8080/auth/facebook/callback'
+        'clientID'      : '294994240849508', // your App ID
+        'clientSecret'  : '1d23f4e1a3775066d96846dddb2918f3', // your App Secret
+        'callbackURL'   : 'http://localhost:8080/auth/facebook/callback'
     },
 
     'twitterAuth' : {
-        'consumerKey'        : 'your-consumer-key-here',
-        'consumerSecret'     : 'your-client-secret-here',
-        'callbackURL'        : 'http://localhost:8080/auth/twitter/callback'
+        'consumerKey'       : 'hje9B1vDoo0K4WHX9skCtf4rR',
+        'consumerSecret'    : 'tP8bkfGGZMqlp1O4zDN5JhT7p23sd0f6lmBqbN3mBZFkzFSixE',
+        'callbackURL'       : 'http://localhost:8080/auth/twitter/callback'
     },
 
     'googleAuth' : {
-        'clientID'         : 'your-secret-clientID-here',
-        'clientSecret'     : 'your-client-secret-here',
-        'callbackURL'      : 'http://localhost:8080/auth/google/callback'
+        'clientID'      : '377876356818-224mqt8htbhpu48d15df99tih6ggonsi.apps.googleusercontent.com',
+        'clientSecret'  : '13b5xSywzVIgR1CFbKy-6K94',
+        'callbackURL'   : 'http://localhost:8080/auth/google/callback'
     }
 
 };

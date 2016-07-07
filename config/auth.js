105cab5f07d48061c849d31fe49b524085f876ed
zhisheng zhou <zz676@nyu.edu>
2016-07-06 22:41:03 -0700

    changes to auth.js

--- config/auth.js
+++ config/auth.js
@@ -5,19 +5,19 @@ module.exports = {
     'facebookAuth' : {
         'clientID'      : '294994240849508', // your App ID
         'clientSecret'  : '1d23f4e1a3775066d96846dddb2918f3', // your App Secret
-        'callbackURL'   : 'http://localhost:8080/auth/facebook/callback'
+        'callbackURL'   : 'https://immense-wildwood-98958.herokuapp.com/auth/facebook/callback'
     },
 
     'twitterAuth' : {
         'consumerKey'       : 'hje9B1vDoo0K4WHX9skCtf4rR',
         'consumerSecret'    : 'tP8bkfGGZMqlp1O4zDN5JhT7p23sd0f6lmBqbN3mBZFkzFSixE',
-        'callbackURL'       : 'http://localhost:8080/auth/twitter/callback'
+        'callbackURL'       : 'https://immense-wildwood-98958.herokuapp.com/auth/twitter/callback'
     },
 
     'googleAuth' : {
         'clientID'      : '377876356818-224mqt8htbhpu48d15df99tih6ggonsi.apps.googleusercontent.com',
         'clientSecret'  : '13b5xSywzVIgR1CFbKy-6K94',
-        'callbackURL'   : 'http://localhost:8080/auth/google/callback'
+        'callbackURL'   : 'https://immense-wildwood-98958.herokuapp.com/auth/google/callback'
     }
 
 };
